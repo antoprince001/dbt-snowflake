b2332e047e58e03bbcde83a69460bb68342d7c46
Antony Prince <antonprince001@gmail.com>
2022-11-11 18:41:20 +0000

    Edit macros

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,24 +1,27 @@
-select 
+select
 --from raw_orders
-o.orderid,
-o.orderdate,
-o.shipdate,
-o.shipmode,
-o.ordersellingprice - o.ordercostprice as orderprofit,
-o.ordercostprice,
-o.ordersellingprice,
+orderid,
+orderdate,
+shipdate,
+shipmode,
+o.customerid,
+o.productid,
+ordersellingprice,
+ordercostprice,
 --from raw_customer
-c.customerid,
-c.customername,
-c.segment,
-c.country,
+customername,
+segment,
+country,
 --from raw_product
-p.productid,
-p.category,
-p.productname,
-p.subcategory
+category,
+productname,
+subcategory,
+ordersellingprice - ordercostprice as orderprofit, {{ markup('ordersellingprice','ordercostprice') }} as markup
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
 on o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p
 on o.productid = p.productid
+
+
+{{ limit_data_in_dev('orderdate') }}
