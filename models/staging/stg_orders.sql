3021d0cd5a267f08be80bd80a54ae3a63b1f6bd9
Antony Prince <antonprince001@gmail.com>
2022-11-11 18:58:26 +0000

    Add delivery_team seed

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,28 +1,31 @@
 select
 --from raw_orders
 {{ dbt_utils.surrogate_key(['o.orderid', 'c.customerid','p.productid']) }} as sk_orders,
-orderid,
-orderdate,
-shipdate,
-shipmode,
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
 o.customerid,
 o.productid,
-ordersellingprice,
-ordercostprice,
+o.ordersellingprice,
+o.ordercostprice,
 --from raw_customer
-customername,
-segment,
-country,
+c.customername,
+c.segment,
+c.country,
 --from raw_product
-category,
-productname,
-subcategory,
-ordersellingprice - ordercostprice as orderprofit, {{ markup('ordersellingprice','ordercostprice') }} as markup
+p.category,
+p.productname,
+p.subcategory,
+o.ordersellingprice - o.ordercostprice as orderprofit, {{ markup('ordersellingprice','ordercostprice') }} as markup,
+-- from delivery_team
+d.delivery_team
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
 on o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p
 on o.productid = p.productid
-
+left join {{ ref('delivery_team') }} as d
+on o.shipmode = d.shipmode
 
 {{ limit_data_in_dev('orderdate') }}
